1ab14e2dfae59636222ba6f6fd2def9def29356f
Follyjay <108089603+Follyjay@users.noreply.github.com>
2023-05-02 23:42:09 +0100

    Excercise-6 solution
    getting value from an input-text and displaying it in an alert using js

--- week4/script.js
+++ week4/script.js
@@ -1,21 +1,9 @@
-function updateParagraph() {
-    var element = document.getElementById('paragraph');
-    element.firstChild.nodeValue = 'New Heading';
+function getValue(){
+    var element = document.getElementById('txtInput');
+    alert(element.value);
 }
-function updateH1() {
-    var element = document.getElementById('pageheading');
-    element.firstChild.nodeValue = 'New paragraph contents';
-}
-function reverseH1() {
-    var element = document.getElementById('pageheading');
-    element.firstChild.nodeValue = 'Page content';
-}
-function myLoadFunction() {
-    var element = document.getElementById('pageheading');
-    element.addEventListener('mouseenter', updateH1);
-    element.addEventListener('mouseleave', reverseH1);
-    
-    var element = document.getElementById('paragraph');
-    element.addEventListener('click', updateParagraph);
+function myLoadFunction(){
+    var element = document.getElementById('btnSubmit');
+    element.addEventListener('click', getValue);
 }
 document.addEventListener('DOMContentLoaded', myLoadFunction);
