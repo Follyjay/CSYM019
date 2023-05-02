c4358431450c9f5581223ce650885cfcd3b07121
Follyjay <108089603+Follyjay@users.noreply.github.com>
2023-05-02 23:29:47 +0100

    week-4 assignment-5 solution
    using click event, mouseleave and mouseenter event to change and reverse the content of an HTML element

--- week4/script.js
+++ week4/script.js
@@ -1,9 +1,21 @@
+function updateParagraph() {
+    var element = document.getElementById('paragraph');
+    element.firstChild.nodeValue = 'New Heading';
+}
+function updateH1() {
+    var element = document.getElementById('pageheading');
+    element.firstChild.nodeValue = 'New paragraph contents';
+}
+function reverseH1() {
+    var element = document.getElementById('pageheading');
+    element.firstChild.nodeValue = 'Page content';
+}
 function myLoadFunction() {
     var element = document.getElementById('pageheading');
-    element.firstChild.nodeValue = 'New Heading';
+    element.addEventListener('mouseenter', updateH1);
+    element.addEventListener('mouseleave', reverseH1);
     
     var element = document.getElementById('paragraph');
-    element.firstChild.nodeValue = 'New paragraph text';
+    element.addEventListener('click', updateParagraph);
 }
 document.addEventListener('DOMContentLoaded', myLoadFunction);
-   
