822e5f54b3ab7de83589beb2a575e20f6715f49b
Follyjay <108089603+Follyjay@users.noreply.github.com>
2023-05-04 07:37:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function clickMeFunction(){
-    alert('The button was pressed');
-}
-function clickFuntion(){
-    var element = document.getElementById('circle');
-    element.addEventListener('click', clickMeFunction)
-}
-document.addEventListener('DOMContentLoaded', clickFuntion);
