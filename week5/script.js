8110f29f983e2a3bb4d602fe4e6430d578fa7201
Follyjay <108089603+Follyjay@users.noreply.github.com>
2023-05-04 09:06:34 +0100

    Exercise-6 solution

--- week5/script.js
+++ week5/script.js
@@ -4,9 +4,12 @@ function clickMeFunction(){
     element.style.opacity = changeopacity - 0.1;
     
 }
+function interval(){
+    setInterval(clickMeFunction, 1000);
+}
 function clickFuntion(){
     var element = document.getElementById('circle');
     element.style.opacity = 1;
-    element.addEventListener('click', clickMeFunction)
+    element.addEventListener('click', interval)
 }
 document.addEventListener('DOMContentLoaded', clickFuntion);
